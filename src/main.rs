483c95ed6181358057e6088cd1779481015411e0
Kyle McKean <mckean.kylej@gmail.com>
2018-01-26 15:09:27 -0800

    more stuff

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+use std::fmt;
+use std::fmt::{Display, Formatter};
+
+enum Arg<'a> {
+    Short(&'a str),
+    Long(&'a str),
+    Name(&'a str),
+}
+
+enum Expr<'a> {
+    Command {
+        name: &'a str,
+        args: Vec<Arg<'a>>
+    },
+}
+
+impl<'a> Display for Expr<'a> {
+    fn fmt(&self, format: &mut Formatter) -> fmt::Result {
+        use self::Expr::*;
+        match *self {
+            Command { name, args } => write!(format, "{}", name),
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let x = Expr::Command {
+        name: "ping",
+        args: vec![],
+    };
+    println!("{}", x);
 }
